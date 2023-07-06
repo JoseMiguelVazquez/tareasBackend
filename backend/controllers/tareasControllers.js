4a1674641922a691e498f3f515e648c1b0bf0da3
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-07-05 21:21:57 -0600

    async-handler

--- backend/controllers/tareasControllers.js
+++ backend/controllers/tareasControllers.js
@@ -1,25 +1,28 @@
-const getTareas = (req, res) => {
-    res.status(200).json({message: 'Get Tareas'})
-}
+const asyncHandler = require('express-async-handler')
 
-const createTareas = (req, res) => {
+
+const getTareas = asyncHandler( async (req, res) => {
+    res.status(200).json({message: 'Get Tareas'})
+})
+
+const createTareas = asyncHandler( async (req, res) => {
 
     if(!req.body.texto) {
         res.status(400)
         throw new Error('Por favor teclea una descripcion a la tarea')
     }
-        // console.log(req.body.texto)
-        res.status(201).json({message: 'Crear Tareas'})
+    // console.log(req.body.texto)
+    res.status(201).json({message: 'Crear Tareas'})
         
-}
+})
 
-const updateTareas = (req, res) => {
+const updateTareas = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Modificar la tarea con id ${req.params.id}`})
-}
+})
 
-const deleteTareas = (req, res) => {
+const deleteTareas = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Borrar la tarea con id ${req.params.id}`})
-}
+})
 
 module.exports = {
     getTareas,
